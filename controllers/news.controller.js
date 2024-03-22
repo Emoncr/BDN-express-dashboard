4f325806f02f415b00be02247f1b732a690b70e0
Emoncr <biplobemon 75945@gmail.com>
2024-03-23 04:53:38 +0600

    ADD - handle news creating in DB

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,20 +1,28 @@
 import { PrismaClient } from "@prisma/client";
-import {
-  CloudinaryMultipleFileUpload,
-  uploadToCloudinary,
-} from "../utils/helper.js";
-
+import { CloudinaryMultipleFileUpload } from "../utils/FileUpload.js";
+import { throwError } from "../utils/helper.js";
 const prisma = new PrismaClient();
 
 export const createNews = async (req, res, next) => {
   try {
-    console.log(req.files);
-    //   const imgurl = await uploadToCloudinary(req.file);
-
+    const reqBody = req.body;
     const imgUrls = await CloudinaryMultipleFileUpload(req.files.coverimage);
-    
+
+    if (!imgUrls[0])
+      return next(throwError(400, "Please upload at least 1 images"));
+    reqBody.img1 = imgUrls[0] ? imgUrls[0] : "";
+    reqBody.img2 = imgUrls[1] ? imgUrls[1] : "";
+    reqBody.img3 = imgUrls[2] ? imgUrls[2] : "";
+    reqBody.img4 = imgUrls[3] ? imgUrls[3] : "";
+
+
+    const news = await prisma.News_list.create({
+      data: reqBody,
+    });
+
+    res.status(200).json({ success: true, data: news });
   } catch (error) {
-    console.log(error);
+    console.log(error.toString());
     next(error);
   }
 };
